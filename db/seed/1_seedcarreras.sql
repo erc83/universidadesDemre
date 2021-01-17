782ccd81e30a52ec0e0a9ccb2ef0aa1493b15729
erc83 <espinoza.eric@gmail.com>
2021-01-17 16:54:02 -0300

    falta levantar server

---
+++ db/seed/1_seedcarreras.sql
@@ -0,0 +1,1 @@
+COPY carreras FROM '/home/erc83/weekendCode/univeridadesDemre/db/migrations/csv/ofertpregrado2021.csv' csv HEADER;
--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const morgan= require('morgan');
 const app = express();
-//const { getUniversidades } = require ("./db/index") // teniendo las bases de datos la utilizamos
-
 
 //configurnado el server
 app.set('port', process.env.PORT || 3000)
@@ -17,11 +15,20 @@ app.use(express.json())
 //rutas
 
 app.get('/', (req,res)=>{
-    res.send({ 'soy la ruta raiz' })
+    res.json({'title': 'hello word'})
 })
 
+app.post('/', (req,res)=>{
+    res.json({'title': 'hello word'})
+})
 
+app.put('/', (req,res)=>{
+    res.json({'title': 'hello word'})
+})
 
+app.delete('/', (req,res)=>{
+    res.json({'title': 'hello word'})
+})
 
 app.listen(3000,()=>{
     console.log(`servidor ${app.get('port')} funcionando ok`)
