7a3b477ef7017526376f7381202067e3f222d263
erc83 <espinoza.eric@gmail.com>
2021-01-17 03:12:14 -0300

    mejorando master

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const morgan= require('morgan');
 const app = express();
+//const { getUniversidades } = require ("./db/index") // teniendo las bases de datos la utilizamos
+
 
 //configurnado el server
 app.set('port', process.env.PORT || 3000)
